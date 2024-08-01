901a083ae3e194b49c1f7843772802e8bb812b1c
sefayilmaz <sefaylmaz14@gmail.com>
2024-08-01 17:02:02 +0300

    Add project files.

---
+++ PatikaWeek4PrtaikIlkSinifimiziOlusturuyoruz/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PrtaikIlkSinifimiziOlusturuyoruz
+{
+    public class Person
+    {
+        //Field tanımlamaları
+        private string name;
+        private string surname;
+        private string birthDate;
+        private string role;
+
+        //Propetry Tanımlamaları
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+                
+            }
+        }
+
+        public string BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+            set
+            {
+
+                birthDate = value;
+                
+
+            }
+        }
+
+        public string Role
+        {
+            get
+            {
+                return role;
+            }
+            set
+            {
+                role = value;
+            }
+        }
+        //Konsola yazdırma methodu
+            public void Display()
+        {
+            Console.WriteLine($"{role}'nin ismi:{name} Soyismi:{surname} Doğum Tarihi:{birthDate}");
+        }
+
+        
+    }
+}
+
---
+++ PatikaWeek4PrtaikIlkSinifimiziOlusturuyoruz/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using PatikaWeek4PrtaikIlkSinifimiziOlusturuyoruz;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person student1 = new Person();//Nesne oluşturma
+        //Tanımlamaların yapılması
+        student1.Name = "Ahmet";
+        student1.Surname = "Çavuş";
+        student1.BirthDate = "12.12.1995";
+        student1.Role = "Öğrenci";
+
+        Person student2 = new Person();//Nesne oluşturma
+        //Tanımlamaların yapılması
+        student2.Name = "Ayşe";
+        student2.Surname = "Yolgezen";
+        student2.BirthDate = "24.07.1995";
+        student2.Role = "Öğrenci";
+
+        Person teacher1 = new Person();//Nesne oluşturma
+        //Tanımlamaların yapılması
+        teacher1.Name = "Gülsüm";
+        teacher1.Surname = "Bakır";
+        teacher1.BirthDate = "13.05.1970";
+        teacher1.Role = "Öğretmen";
+        //Method yardımı ile konsola yazdırma
+        student1.Display();
+        student2.Display();
+        teacher1.Display();
+    }
+}
